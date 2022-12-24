56285251eb7ec2b0d2d0946c665eb32a3e1e6463
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-12-24 17:08:22 +0100

    Configure devise views for scoped views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@
 Devise.setup do |config|
 
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.scoped_views = true
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
