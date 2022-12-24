a3597b321329b8147b316f76b13db7600899dcf8
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-12-24 19:28:01 +0100

    Add role ENUM to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  enum :role, [ :user, :lecturer ], default: :user
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
