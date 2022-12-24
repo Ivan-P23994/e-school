21a23ebd2ba055c145dc1c318d9219b678226a66
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-12-24 16:51:42 +0100

    Set up initial active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   get 'dashboard/index'
+
   devise_for :users
 
   root to: "dashboard#index"
