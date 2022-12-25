08a31c42b84559a719bd99c671b18c56b1ff3d8d
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-12-25 17:47:10 +0100

    Add basic user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :first_name, presence: true, length: 2..20
+  validates :last_name, presence: true, length: 2..20
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX, message: 'Email is invalid' }
+
+
 end
