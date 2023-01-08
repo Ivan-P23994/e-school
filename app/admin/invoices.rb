2ffdc7bd08bf0520b1789489c1512233e2f26ae3
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2023-01-08 15:47:07 +0100

    Generate active_admin invoice resource

---
+++ app/admin/invoices.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Invoice do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :invoice_no, :user_id, :title, :amount, :paid_amount, :status
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:invoice_no, :user_id, :title, :amount, :paid_amount, :status]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
