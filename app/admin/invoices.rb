bcc26ce7e31af31aa2bb940f5a6421bdd86a4a0c
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2023-01-10 21:13:15 +0100

    Set up invoice columns secondary values, status indicator, header title and pagination config

--- app/admin/invoices.rb
+++ app/admin/invoices.rb
@@ -1,18 +1,39 @@
 ActiveAdmin.register Invoice do
+  config.per_page = [10, 50, 100]
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :invoice_no, :user_id, :title, :amount, :paid_amount, :status
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:invoice_no, :user_id, :title, :amount, :paid_amount, :status]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :invoice_no, :user_id, :title, :amount, :paid_amount, :status
+
+  index title: 'Student Fee Manager' do
+    selectable_column
+    column :invoice_no
+    column 'Invoicee' do |invoice|
+      div for: invoice do
+        em invoice.user.full_name; br
+        strong 'Course:' 
+        unless invoice.user.courses.first.nil?
+          span invoice.user.courses.first.title.to_s
+        end
+      end
+    end
+    column :title
+    column 'Amount' do |invoice|
+      div for: invoice do
+        span "$ " + invoice.amount.to_s; br
+        strong 'Created at: ' 
+        span invoice.created_at.to_date
+      end
+    end
+    column 'Paid Amount' do |invoice|
+      div for: invoice do
+        span "$ " + invoice.paid_amount.to_s; br
+        strong 'Payment Date: '
+        span "Not Found"
+      end
+    end
+    column :status do |invoice|
+      status_tag invoice.status
+    end
+    actions
+  end
+
 end
