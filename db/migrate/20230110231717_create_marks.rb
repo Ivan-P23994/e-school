ca4afb624b4b5f3ea3ef4d76d604cf9d47a997bf
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2023-01-11 00:29:00 +0100

    Generate Mark resource

---
+++ db/migrate/20230110231717_create_marks.rb
@@ -0,0 +1,12 @@
+class CreateMarks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :marks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :comment
+      t.integer :mark
+      t.integer :grade_point
+
+      t.timestamps
+    end
+  end
+end
