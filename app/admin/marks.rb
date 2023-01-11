4dd552e13ead2f606c50b3e0451dfe24b7b935b1
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2023-01-11 01:14:17 +0100

    Generate marks active_admin resource

---
+++ app/admin/marks.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Mark do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :comment, :mark, :grade_point
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :comment, :mark, :grade_point]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
