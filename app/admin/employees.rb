6106467baead679b4abc4c42e81b2de7f108968f
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2023-01-30 23:11:49 +0100

    Add XLSX import for Employee resource

--- app/admin/employees.rb
+++ app/admin/employees.rb
@@ -17,21 +17,51 @@ ActiveAdmin.register Employee do
   filter :first_name
   filter :last_name
   filter :email
-  
-  collection_action :upload_csv do
+
+  action_item only: :index do
+    link_to 'Export XLS', action: 'export_xlsx'
+  end
+
+  collection_action :export_xlsx, method: :get do
+    @employees = Employee.all
+    respond_to do |format|
+      format.xlsx
+    end
+  end
+
+  # XLSX IMPORT #
+
+  action_item only: :index do
+    link_to 'Import XLSX', action: 'open_xls_picker'
+  end
+
+  collection_action :open_xls_picker do
+    render 'admin/xls/upload_xls'
+  end
+
+  collection_action :import_xls, method: :post do
+    XlsHelper.convert_to_employees(params[:dump][:file])
+    redirect_to({ action: :index }, notice: 'XLSX imported successfully!')
+
+  rescue StandardError
+    redirect_to({ action: :index },
+                flash: { error: 'XLSX imported failed! Check the template is correct or contact a developer.' })
+  end
+
+  # CSV IMPORT #
+
+  action_item only: :index do 
+    link_to 'Import CSV', action: 'open_csv_picker'
+  end
+
+  collection_action :open_csv_picker do
     render 'admin/csv/upload_csv'
   end
 
-  action_item only: :index do
-    link_to 'Upload CSV', action: 'upload_csv'
-  end
-
   collection_action :import_csv, method: :post do
-    employees = CsvHelper.convert_to_employees(params[:dump][:file])
-    Employee.transaction do
-      employees.each(&:save!)
-    end
+    CsvHelper.convert_to_employees(params[:dump][:file])
     redirect_to({ action: :index }, notice: 'CSV imported successfully!')
+
   rescue StandardError
     redirect_to({ action: :index },
                 flash: { error: 'CSV imported failed! Check the template is correct or contact a developer.' })
